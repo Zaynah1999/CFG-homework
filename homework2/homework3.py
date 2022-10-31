83a63073004aee861dfc57f90c129fea77115f58
Zaynah Ahmed <azaynah295@gmail.com>
2022-10-31 16:35:57 +0000

    homework 3 and 4

---
+++ homework2/homework3.py
@@ -0,0 +1,61 @@
+"""
+SEARCH IN MATRIX
+--------
+
+You are give a matrix (a list of lists) of DISTINCT integers and a target number.
+Each row in the matrix is SORTED and each column in the matrix is SORTED.
+Our matrix does not necessarily have the same height and width.
+
+Write a function that returns a list of the row and column indices of the target integer
+IF it is contained in the matrix, otherwise return [-1, -1].
+
+EXAMPLE INPUT
+
+matrix = [
+[1,4,7,12,15,1000],
+[2,5,19,31,32,1001],
+[3,8,24,33,35,1002],
+[40,41,42,44,45,1003],
+[99,100,103,106,128,1004]
+]
+
+target =44
+
+EXAMPLE OUTPUT
+
+result = [3,3]
+
+print(f"[{row},{col}]")
+
+"""
+#
+matrix = [
+[1,4,7,12,15,1000],
+[2,5,19,31,32,1001],
+[3,8,24,33,35,1002],
+[40,41,42,44,45,1003],
+[99,100,103,106,128,1004]
+]
+
+target = 908
+
+def search_in_matrix(matrix, target):
+    array = None
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            if matrix[row][col] == target:
+                array = [row, col]
+    if array != None:
+        return array
+    else:
+        return [-1,-1]
+
+print(search_in_matrix(matrix, target))
+
+
+
+
+
+
+
+
